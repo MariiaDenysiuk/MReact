5ee3ddd6a58a7e43a91d6aa17b3257607c82caad
maryden <marydenysiuk@gmail.com>
2020-03-01 23:19:09 +0200

    added express, babel transpiler

---
+++ app.js
@@ -0,0 +1,11 @@
+/** @jsx MReact.createElement */ 
+
+const root = document.getElementById("root");
+
+var Step1 = (
+    <div>
+        <h1 class="className">Hello tine</h1>
+    </div>
+);
+
+console.log(Step1);
