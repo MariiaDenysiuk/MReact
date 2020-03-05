a12170e18ae007912822f543ce7602b2cefe68f7
maryden <marydenysiuk@gmail.com>
2020-03-05 07:20:47 +0200

    added diff function

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ const root = document.getElementById("root");
 var Step1 = (
     <div>
         <h1 class="className">Hello tine</h1>
+        <p onClick={()=> alert("Hello")}>Click</p>
     </div>
 );
 
-console.log(Step1);
+var Step2 = (
+    <div>
+        <h1 class="className">Hello Masha</h1>
+        <p onClick={()=> alert("Hello")}>Click</p>
+    </div>
+);
 MReact.render(Step1, root);
+
+setTimeout(() => {
+    alert("Re-rendering");
+    MReact.render(Step2, root);
+}, 4000);
